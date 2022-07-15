377b88402b2f3d229eb623e1d1ff200168f94515
Katie Byers <lobsterkatie@gmail.com>
2022-07-15 00:07:26 -0700

    add loader

---
+++ packages/nextjs/src/config/loader.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+// TODO what to do about the other two options for what GSSP can return (redirect, notFound)?
+// TODO Deal with _app separately
+// TODO strip extension from route name
+
+type LoaderThis = {
+  resourcePath: string;
+  getOptions: () => {
+    distDir: string;
+    projectDir: string;
+    pagesDirRegex: RegExp;
+  };
+};
+
+/**
+ *
+ */
+function addSentryToSource(this: LoaderThis, userCode: string): string {
+  // @ts-ignore TODO fix me
+  const loaderThis = this as LoaderThis;
+  const { distDir, pagesDirRegex } = loaderThis.getOptions();
+
+  const route = loaderThis.resourcePath.match(pagesDirRegex)?.[2];
+  debugger;
+
+  const templatePath = path.resolve(__dirname, 'injectionTemplate.js');
+  let templateCode = fs.readFileSync(templatePath).toString();
+
+  // Remove this so as not to confuse anything in the final bundles
+  templateCode = templateCode.replace(/.*sourceMappingURL.*/, '');
+
+  // Fill in placeholders
+  templateCode = templateCode.replace('__FILEPATH__', route);
+  templateCode = templateCode.replace('__DIST_DIR__', distDir);
+
+  debugger;
+
+  return `${userCode}\n${templateCode}`;
+}
+
+export { addSentryToSource as default };
