3e8b1252713b030bc513e1e0c32b555be8c2f109
Katie Byers <lobsterkatie@gmail.com>
2022-07-15 00:11:26 -0700

    add extra files to build

--- packages/nextjs/rollup.npm.config.js
+++ packages/nextjs/rollup.npm.config.js
@@ -4,9 +4,15 @@ export default makeNPMConfigVariants(
   makeBaseNPMConfig({
     // We need to include `instrumentServer.ts` separately because it's only conditionally required, and so rollup
     // doesn't automatically include it when calculating the module dependency tree.
-    entrypoints: ['src/index.server.ts', 'src/index.client.ts', 'src/utils/instrumentServer.ts'],
+    entrypoints: [
+      'src/index.server.ts',
+      'src/index.client.ts',
+      'src/utils/instrumentServer.ts',
+      'src/config/injectionTemplate.js',
+      'src/config/loader.ts',
+    ],
     // prevent this nextjs code from ending up in our built package (this doesn't happen automatially because the name
     // doesn't match an SDK dependency)
-    packageSpecificConfig: { external: ['next/router'] },
+    packageSpecificConfig: { external: ['next/router', '@sentry/nextjs'] },
   }),
 );
